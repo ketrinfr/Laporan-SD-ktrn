ae9f4753a88831611974c02c56b7c97700c97004
ketrinfr <148761161+ketrinfr@users.noreply.github.com>
2024-03-26 01:25:55 +0700

    lat job 3

---
+++ 3.2.cpp
@@ -0,0 +1,31 @@
+//crated by ketherin fathur rahma
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int data;
+	struct node *next;
+};
+
+int main()
+{
+	struct node* head = NULL;
+	struct node* second = NULL;
+	struct node* third = NULL;
+	
+	head = (struct node*)malloc(sizeof(struct node));
+	second = (struct node*)malloc(sizeof(struct node));
+	third = (struct node*)malloc(sizeof(struct node));
+	
+	head->data = 1;
+	head->next = second;
+	
+	second->data = 2;
+	second->next = third;
+	
+	third->data = 3;
+	third->next = NULL;
+	
+	return 0;
+}
